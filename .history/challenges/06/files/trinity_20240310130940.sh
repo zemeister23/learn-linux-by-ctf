f497dd309b368dffb6d5100a6074bca6fd226ca5
zemeister23 <sauda2332@gmail.com>
2024-03-10 13:18:28 +0500

    challenge06 new

---
+++ .history/challenges/06/files/trinity_20240310130940.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Prompt the user with the question
+read -p "What is Linux? " answer
+
+# Check if the user's answer is "kernel"
+if [ "$answer" = "kernel" ]; then
+    # Display the flag
+    echo "Congratulations! Here is the flag: FLAG_L1NUX_K3RNEL"
+else
+    echo "Incorrect answer. Keep exploring!"
+fi
---
+++ .history/challenges/06/files/trinity_20240310131109.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Prompt the user with the question
+echo "Only lower cases for answers!" 
+read -p "What is Linux? " answer
+
+# Check if the user's answer is "kernel"
+if [ "$answer" = "kernel" ]; then
+    # Display the flag
+    echo "Congratulations! Here is your flag location: "
+else
+    echo "Incorrect answer. Keep exploring!"
+fi
