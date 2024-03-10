3954807f24deedf815b1be9295ccd7a115acf9df
zemeister23 <sauda2332@gmail.com>
2024-03-10 18:16:23 +0500

    sdsf

--- .history/challenges/07/files/challenge07_20240310163823.c
+++ .history/challenges/07/files/challenge07_20240310163823.c
@@ -10,12 +10,12 @@ int main() {
 
     // Use stat to retrieve file information
     if (stat(filename, &file_stat) == 0) {
-        // // Check if file permissions are 744
-        // if ((file_stat.st_mode & S_IRWXU) == S_IRUSR|S_IWUSR|S_IXUSR && (file_stat.st_mode & S_IRGRP) && (file_stat.st_mode & S_IROTH)) {
-        //     printf("Flag: {{flag}}\n");
-        // } else {
-        //     printf("File permissions are not set to 744\n");
-        // }
+        // Check if file permissions are 744
+        if ((file_stat.st_mode & S_IRWXU) == S_IRUSR|S_IWUSR|S_IXUSR && (file_stat.st_mode & S_IRGRP) && (file_stat.st_mode & S_IROTH)) {
+            printf("Flag: {{flag}}\n");
+        } else {
+            printf("File permissions are not set to 744\n");
+        }
     } else {
         printf("Try Harder Neo, Never Give Up!\n");
         perror("stat");
